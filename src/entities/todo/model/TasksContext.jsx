c353d5ed251d583b87fd5ee94d1f299c86432f3a
YurySinitsiy <unstable195@gmail.com>
2026-02-15 19:57:20 +0300

    create app

---
+++ src/entities/todo/model/TasksContext.jsx
@@ -0,0 +1,64 @@
+import { createContext } from "react";
+import useTasks from "./useTasks";
+import useIncompleteTaskScroll from "./useIncompleteTaskScroll";
+import { useMemo } from "react";
+
+export const TasksContext = createContext({});
+
+export const TasksProvider = (props) => {
+  const { children } = props;
+
+  const {
+    tasks,
+    filteredTasks,
+    deleteAllTasks,
+    deleteTask,
+    toggleTaskComplete,
+    searchQuery,
+    setSearchQuery,
+    newTaskInputRef,
+    addTask,
+    disappearingTaskId,
+    appearingTaskId,
+  } = useTasks();
+
+  const { firstIncompleteTaskRef, firstIncompleteTaskId } =
+    useIncompleteTaskScroll(tasks);
+
+  const value = useMemo(
+    () => ({
+      tasks,
+      filteredTasks,
+      deleteAllTasks,
+      deleteTask,
+      toggleTaskComplete,
+      searchQuery,
+      setSearchQuery,
+      newTaskInputRef,
+      addTask,
+      disappearingTaskId,
+      appearingTaskId,
+      firstIncompleteTaskRef,
+      firstIncompleteTaskId,
+    }),
+    [
+      tasks,
+      filteredTasks,
+      deleteAllTasks,
+      deleteTask,
+      toggleTaskComplete,
+      searchQuery,
+      setSearchQuery,
+      newTaskInputRef,
+      addTask,
+      disappearingTaskId,
+      appearingTaskId,
+      firstIncompleteTaskRef,
+      firstIncompleteTaskId,
+    ],
+  );
+
+  return (
+    <TasksContext.Provider value={value}>{children}</TasksContext.Provider>
+  );
+};
